03f2898bb4c579321d69edaaa7171cbe7b8e7217
Veronika Dmitrieva <veronika.dmitrieva.1990@mail.ru>
2026-02-18 18:11:57 +0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import './App.css'
 import {Header} from "./components/Header/Header.tsx";
 import {Navbar} from "./components/Navbar/Navbar.tsx";
 import {Dialogs} from "./components/Dialogs/Dialogs.tsx";
+import {Route, Routes} from "react-router";
+import {Profile} from "./components/Profile/Profile.tsx";
+import {News} from "./components/News/News.tsx";
+import {Music} from "./components/Music/Music.tsx";
+import {Settings} from "./components/Settings/Settings.tsx";
 
 
 export const App = () => {
@@ -9,9 +14,15 @@ export const App = () => {
     return <div className="app-wrapper">
         <Header/>
         <Navbar/>
-        {/*<Profile/>*/}
         <div className="app-wrapper-content">
-            <Dialogs/>
+            <Routes>
+                <Route path={'/dialogs'} element={<Dialogs/>}/>
+                <Route path={'/profile'} element={<Profile/>}/>
+                <Route path={'/news'} element={<News/>}/>
+                <Route path={'/music'} element={<Music/>}/>
+                <Route path={'/settings'} element={<Settings/>}/>
+            </Routes>
+
         </div>
 
     </div>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,23 @@
 import s from "./Navbar.module.css"
+import {Link} from "react-router";
 
 export const Navbar = () => {
     return <nav className={s.nav}>
-        <div className={s.item}><a>Profile</a></div>
-        <div className={s.item}><a>Messages</a></div>
-        <div className={s.item}><a>News</a></div>
-        <div className={s.item}><a>Music</a></div>
-        <div className={s.item}><a>Settings</a></div>
+        <div className={s.item}>
+            <Link to="/profile">Profile</Link>
+        </div>
+        <div className={s.item}>
+            <Link to="/dialogs">Dialogs</Link>
+        </div>
+        <div className={s.item}>
+            <Link to="/news">News</Link>
+        </div>
+        <div className={s.item}>
+            <Link to="/music">Music</Link>
+        </div>
+        <div className={s.item}>
+            <Link to="/settings">Settings</Link>
+        </div>
     </nav>
 }
 
