cd61d300b7f837d4dc8beba2f37c34710846cabf
Veronika Dmitrieva <veronika.dmitrieva.1990@mail.ru>
2026-02-19 23:12:27 +0300

    ÐDialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,24 @@
-
+import s from "./Dialogs.module.css"
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+
+            <div className={s.dialogsItems}>
+
+                <div className={s.dialog + ' ' + s.active}>Veronica</div>
+                <div className={s.dialog}>Sveta</div>
+                <div className={s.dialog}>Dima</div>
+                <div className={s.dialog}>Victor</div>
+
+            </div>
+
+            <div className={s.messages}>
+                <div className={s.message}>Hello!</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>What are you doing tonight?</div>
+            </div>
+
         </div>
     )
 }
