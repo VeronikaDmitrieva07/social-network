cebd59cdb7b687d58e839e823cf78ec02fb53681
Veronika Dmitrieva <veronika.dmitrieva.1990@mail.ru>
2026-02-20 16:18:27 +0300

    refactoring Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,26 +2,39 @@ import s from "./Dialogs.module.css"
 import {DialogItem} from "./DialogItem/DialogItem.tsx";
 import {Message} from "./Message/Message.tsx";
 
-
-
 export const Dialogs = () => {
+
+    const dialogsData = [
+        {id: "1", name: "Veronica"},
+        {id: "2", name: "Sveta"},
+        {id: "3", name: "Dima"},
+        {id: "4", name: "Victor"},
+    ]
+
+    const messagesData = [
+        {id: "1", message: "Hello!"},
+        {id: "2", message: "How are you?"},
+        {id: "3", message: "What are you doing tonight?"},
+        {id: "4", message: "Hi"},
+    ]
+
     return (
         <div className={s.dialogs}>
 
             <div className={s.dialogsItems}>
 
-               <DialogItem name="Veronica" id="1"/>
-               <DialogItem name="Sveta" id="2"/>
-               <DialogItem name="Dima" id="3"/>
-               <DialogItem name="Victor" id="4"/>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
 
             </div>
 
             <div className={s.messages}>
-                <Message message="Hello!"/>
-                <Message message="How are you?"/>
-                <Message message="What are you doing tonight?"/>
-                <Message message="Hi"/>
+                <Message message={messagesData[0].message}/>
+                <Message message={messagesData[1].message}/>
+                <Message message={messagesData[2].message}/>
+                <Message message={messagesData[3].message}/>
             </div>
 
         </div>
