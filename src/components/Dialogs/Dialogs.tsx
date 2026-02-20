e2e019450fcb4f37ecd0a380794c944967aa9b11
Veronika Dmitrieva <veronika.dmitrieva.1990@mail.ru>
2026-02-20 18:26:48 +0300

    add map

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,14 +4,14 @@ import {Message} from "./Message/Message.tsx";
 
 export const Dialogs = () => {
 
-    const dialogsData = [
+    const dialogs = [
         {id: "1", name: "Veronica"},
         {id: "2", name: "Sveta"},
         {id: "3", name: "Dima"},
         {id: "4", name: "Victor"},
     ]
 
-    const messagesData = [
+    const messages = [
         {id: "1", message: "Hello!"},
         {id: "2", message: "How are you?"},
         {id: "3", message: "What are you doing tonight?"},
@@ -22,19 +22,11 @@ export const Dialogs = () => {
         <div className={s.dialogs}>
 
             <div className={s.dialogsItems}>
-
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
-
+                {dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)}
             </div>
 
             <div className={s.messages}>
-                <Message message={messagesData[0].message}/>
-                <Message message={messagesData[1].message}/>
-                <Message message={messagesData[2].message}/>
-                <Message message={messagesData[3].message}/>
+                {messages.map(m => <Message message={m.message}/>)}
             </div>
 
         </div>
